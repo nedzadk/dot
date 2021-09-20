27285f7ffac13c372da538fddddf5655c8ec3ad3
Nedzad Kulelija <nedzadk@gmail.com>
2021-09-20 11:42:53 +0200

    Added iterm theme

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ ln -s ${CONF_LOCATION}/tmux/tmux.conf ~/.tmux.conf
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+open ${CONF_LOCATION}/iterm/nord.itermcolors
