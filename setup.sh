a33c7dce726a7f50adbab2364b22bd7ee983fa96
Nedzad Kulelija <nedzad.kulelija@klarna.com>
2021-10-19 21:16:32 +0200

    Update script premissions

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ echo "Mapping ${CONF_LOCATION}/nvim to ~/.config/nvim"
 rm -rf ~/.config/nvim
 echo "Mapping ${CONF_LOCATION}/tmux/tmux.conf to ~/.tmux.conf"
 rm -rf ~/.tmux.conf
+rm -rf ~/.tmux
 ln -s ${CONF_LOCATION}/nvim ~/.config/nvim
 ln -s ${CONF_LOCATION}/tmux/tmux.conf ~/.tmux.conf
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
