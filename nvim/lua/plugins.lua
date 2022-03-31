8d196634adb67e00d1322eb4d7691d9dbb30def9
Nedzad Kulelija <nedzadk@gmail.com>
2022-03-31 10:46:07 +0200

    Update to neovim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,7 +8,13 @@ end
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/vim-vsnip'
+
   use 'nvim-lua/plenary.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
--- nvim/lua/vars.lua
+++ nvim/lua/vars.lua
@@ -1,6 +1,28 @@
 -- [[ settings.lua ]]
 
 local g = vim.g
+local set = vim.opt
+
 g.t_co = 256
 g.background = "dark"
+
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+set.expandtab = true
+set.smartcase = true
+set.ignorecase = true
+set.breakindent = true
+
+set.cursorline = true
+
+set.updatetime = 250
+set.timeoutlen = 500
+
+set.number = true
+set.relativenumber = true
+
+set.hlsearch = true
+set.signcolumn = "yes"
+set.clipboard = "unnamedplus"
 
